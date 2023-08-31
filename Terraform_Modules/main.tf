4ee0da4b6a1550145e1fd5d088407c6670bb2ead
Ameena-Begam <103177166+Ameena-Begam@users.noreply.github.com>
2023-08-31 12:44:12 +0530

    Add files via upload

---
+++ Terraform_Modules/main.tf
@@ -0,0 +1,18 @@
+module "vpc" {
+  source   = "./vpc"
+  vpc_cidr = var.vpc_cidr
+}
+
+ 
+
+module "s3" {
+  source           = "./s3"
+  s3_bucket_name   = var.s3_bucket_name
+}
+
+ 
+
+module "rds" {
+  source              = "./rds"
+  db_allocated_storage = var.db_allocated_storage
+}
